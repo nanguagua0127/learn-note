6c68719cd68a5774f5af0916fa7c373fe33ca0bb
Nan Gua <znznzn120@163.com>
2021-01-20 14:54:01 +0800

    初始化vue3ts仓库

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img class="bg" src="./../assets/bg.jpg" alt="">
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: {
+  },
+})
+export default class Home extends Vue {}
+</script>
+<style lang="css">
+  .bg {
+    width: 150px;
+    height: auto;
+  }
+</style>
